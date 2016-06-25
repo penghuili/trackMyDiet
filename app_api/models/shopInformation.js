fac066606fe31da8484ca0bfae5f9445e0f7e73d
Yue Chi <yue@Yues-Air.fritz.box>
2016-06-25 10:51:41 +0200

    Updated conceptual model for mongodb

---
+++ app_api/models/shopInformation.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var shopInformationSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true},
+    product: [{type: mongoose.Schema.Types.ObjectId, ref: 'Product'}],
+    price: Number,
+    link: String
+});
+
+mongoose.model('ShopInformation', shopInformationSchema);
