3d3b85ca5298ada209f836a4507a19e7ed874952
Yue Chi <yue@dhcp-10-180-35-44.dynamic.eduroam.mwn.de>
2016-07-03 14:55:52 +0200

    Update add diet programm controller for adding more input fields

--- app_client/adminDietProgramAdd/dietProgramAdd.controller.js
+++ app_client/adminDietProgramAdd/dietProgramAdd.controller.js
@@ -31,3 +31,29 @@ function dietProgramAddCtrl (trackmydietData) {
         });
   };
 }
+
+var counter = 1;
+var limit = 10;
+function addInput(divName){
+  if (counter == limit)  {
+    alert("You have reached the limit of adding " + counter + " inputs");
+  }
+  else {
+    var newdiv = document.createElement('div');
+    newdiv.innerHTML = "Food " + (counter + 1) + " <br><input type='text' name='myInputs[]'>";
+    document.getElementById(divName).appendChild(newdiv);
+    counter++;
+  }
+}
+
+function addPicture(divName){
+  if (counter == limit)  {
+    alert("You have reached the limit of adding " + counter + " pictures");
+  }
+  else {
+    var newdiv = document.createElement('div');
+    newdiv.innerHTML = "Picture " + (counter + 1) + " <br><input name='picture' type='file' size='50' accept='image'>";
+    document.getElementById(divName).appendChild(newdiv);
+    counter++;
+  }
+}
